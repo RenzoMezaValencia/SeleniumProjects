343af1f19983659b47948d6faa508d45bd899300
R3nzoM3za <115664511+RenzoMezaValencia@users.noreply.github.com>
2023-02-09 15:35:50 -0500

    Proyecto JAVA GRALDE SELENIUM

---
+++ SeleniumProject/src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,26 @@
+package steps;
+
+import io.cucumber.java.en.*;
+
+public class GoogleSteps{
+    @Given("^I am on the Google search page$")
+    public void navigateToGoogle(){
+
+    }
+
+    @When("^I enter a serach criteria$")
+    public void enterSearchCrtiteria(){
+
+    }
+
+    @And ("^click on the search button$")
+    public void clickSearchButton(){
+
+    }
+
+    @Then ("^the results match the criteria$")
+    public void validetResults(){
+
+    }
+
+}
